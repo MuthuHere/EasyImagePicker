c7a13133900235b660ccf9eaa76ae81c6c8d5cc8
Yuvaraj <yuvaraj@securenext.net>
2017-09-06 19:35:13 +0530

    1st Commit
    1st Commit
    
     Author:    Yuvaraj <yuvaraj@securenext.net>
     Date:      Wed Sep 6 19:35:13 2017 +0530
     Changes to be committed:
    	new file:   .idea/compiler.xml
    	new file:   .idea/copyright/profiles_settings.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   .idea/runConfigurations.xml
    	new file:   app/.gitignore
    	new file:   app/build.gradle
    	new file:   app/proguard-rules.pro
    	new file:   app/src/androidTest/java/com/colan/easyimagepicker/ExampleInstrumentedTest.java
            new file:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/colan/easyimagepicker/MainActivity.java
    	new file:   app/src/main/java/com/colan/easyimagepicker/MainActivityFragment.java
    	new file:   app/src/main/res/layout/activity_main.xml
    	new file:   app/src/main/res/layout/content_main.xml
    	new file:   app/src/main/res/layout/fragment_main.xml
    	new file:   app/src/main/res/menu/menu_main.xml
    	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-hdpi/ic_launcher_round.png
    	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-mdpi/ic_launcher_round.png
    	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
    	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
    	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
    	new file:   app/src/main/res/values/colors.xml
    	new file:   app/src/main/res/values/dimens.xml
    	new file:   app/src/main/res/values/strings.xml
    	new file:   app/src/main/res/values/styles.xml
    	new file:   app/src/test/java/com/colan/easyimagepicker/ExampleUnitTest.java
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    
     Untracked files:
    	.idea/vcs.xml

---
+++ app/src/main/java/com/colan/easyimagepicker/MainActivity.java
@@ -0,0 +1,123 @@
+package com.colan.easyimagepicker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+import pl.tajchert.nammu.Nammu;
+import pl.tajchert.nammu.PermissionCallback;
+
+import static pl.tajchert.nammu.Nammu.askForPermission;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String PHOTOS_KEY = "easy_image_photos_list";
+
+    private ArrayList<File> photos = new ArrayList<>();
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        image = (ImageView) findViewById(R.id.image);
+
+        if (savedInstanceState != null) {
+            photos = (ArrayList<File>) savedInstanceState.getSerializable(PHOTOS_KEY);
+        }
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            askForPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
+                @Override
+                public void permissionGranted() {
+                    //Nothing, this sample saves to Public gallery so it needs permission
+                }
+
+                @Override
+                public void permissionRefused() {
+                    finish();
+                }
+            });
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EasyImage.openChooserWithGallery(MainActivity.this, "Pick source", 0);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(PHOTOS_KEY, photos);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Nammu.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onImagesPicked(List<File> imageFiles, EasyImage.ImageSource source, int type) {
+                onPhotosReturned(imageFiles);
+            }
+
+            @Override
+            public void onCanceled(EasyImage.ImageSource source, int type) {
+                //Cancel handling, you might wanna remove taken photo if it was canceled
+                if (source == EasyImage.ImageSource.CAMERA) {
+                    File photoFile = EasyImage.lastlyTakenButCanceledPhoto(MainActivity.this);
+                    if (photoFile != null) photoFile.delete();
+                }
+            }
+        });
+    }
+
+    private void onPhotosReturned(List<File> returnedPhotos) {
+        photos.addAll(returnedPhotos);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(returnedPhotos.get(0).getPath());
+        image.setImageBitmap(bitmap);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        // Clear any configuration that was done!
+        EasyImage.clearConfiguration(this);
+        super.onDestroy();
+    }
+}
